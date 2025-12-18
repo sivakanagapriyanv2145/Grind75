05a63b7bd8c900f1fb0d0959ee8bd2cbafd56c5a
sivakanagapriyanv2145 <151417218+sivakanagapriyanv2145@users.noreply.github.com>
2025-12-18 15:40:46 +0530

    Create LC_1.java

---
+++ LC_1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int i;
+        int a[]=new int [2];
+        for(i=0;i<nums.length-1;i++){
+           for(int j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]==target){
+                a[0]=i;
+                a[1]=j;
+            }
+           }
+        }
+        return a;
+        
+    }
+}
