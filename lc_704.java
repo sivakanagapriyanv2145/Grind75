8b7662e14e80843eff043286c04ff4565276da13
sivakanagapriyanv2145 <151417218+sivakanagapriyanv2145@users.noreply.github.com>
2026-01-14 17:41:20 +0530

    Create lc_704.java

---
+++ lc_704.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int i,j;
+        for(i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+}
